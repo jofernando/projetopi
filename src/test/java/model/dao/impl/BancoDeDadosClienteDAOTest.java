a45395afc07c1cddf951062a938d4923afd26276
José Fernando Mendes <fmdinho3@gmail.com>
2017-12-06 12:25:38 -0300

    Corrigindo teste de inserção no banco

--- src/test/java/model/dao/impl/BancoDeDadosClienteDAOTest.java
+++ src/test/java/model/dao/impl/BancoDeDadosClienteDAOTest.java
@@ -5,8 +5,17 @@
  */
 package model.dao.impl;
 
+import helper.ConnectionHelper;
 import helper.DbUnitHelper;
+import java.net.MalformedURLException;
+import java.sql.SQLException;
 import model.entidades.Cliente;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -32,21 +41,24 @@ public class BancoDeDadosClienteDAOTest {
 
     @Test
     public void deveEstarCadastrado() {
-        Cliente amelia = new Cliente("Amelia Pond", "pond@gmail.com", "333.333.333-33", "Escócia", "111.111.111-11");
+        Cliente amelia
+                = new Cliente("Amelia Pond", "pond@gmail.com", "333.333.333-33", "Escócia", "999.999.999-99");
         Assert.assertTrue(bancoDeDadosClienteDAO.estaCadastrado(amelia));
     }
 
     @Test
     public void deveNaoEstarCadastrado() {
-        Cliente hwo = new Cliente("mistery", "tardis@gmail.com", "(12)12312-1231", "Gallifrey", "122.121.423-23");
+        Cliente hwo
+                = new Cliente("mistery", "tardis@gmail.com", "(12)12312-1231", "Gallifrey", "122.121.423-23");
         Assert.assertFalse(bancoDeDadosClienteDAO.estaCadastrado(hwo));
     }
 
     @Test
     public void deveBuscarPorCpf() {
-        Cliente cliente = new Cliente("José", "jose@gmail.com", "(11)11111-1111", "Rua primeira", "111.111.111-11");
+        Cliente cliente
+                = new Cliente("José", "jose@gmail.com", "(88)88888-8888", "Rua oitava", "888.888.888-88");
         cliente.setId(8);
-        Assert.assertEquals(cliente, bancoDeDadosClienteDAO.buscarPorCpf("111.111.111-11"));
+        Assert.assertEquals(cliente, bancoDeDadosClienteDAO.buscarPorCpf("888.888.888-88"));
     }
 
     @Test
@@ -56,7 +68,8 @@ public class BancoDeDadosClienteDAOTest {
 
     @Test
     public void deveBuscar() {
-        Cliente cliente = new Cliente("José", "jose@gmail.com", "(11)11111-1111", "Rua primeira", "111.111.111-11");
+        Cliente cliente
+                = new Cliente("José", "jose@gmail.com", "(88)88888-8888", "Rua oitava", "888.888.888-88");
         cliente.setId(8);
         Assert.assertEquals(cliente, bancoDeDadosClienteDAO.buscar(8));
     }
@@ -67,9 +80,32 @@ public class BancoDeDadosClienteDAOTest {
     }
 
     @Test
-    public void deveInserir() {
+    public void deveInserir() throws DataSetException, SQLException, MalformedURLException, DatabaseUnitException {
         Cliente jonas = new Cliente("Jonas", "jonas@mail.com", "3109231321", "asdasdas", "123.123.423-93");
         Assert.assertTrue(bancoDeDadosClienteDAO.inserir(jonas));
+
+        IDatabaseConnection connection = new DatabaseConnection(ConnectionHelper.getConnection());
+        IDataSet databaseDataSet = connection.createDataSet();
+        ITable actualTable = databaseDataSet.getTable("Cliente");
+
+        Cliente atual = new Cliente();
+        atual.setCpf((String) actualTable.getValue(2, "cpf"));
+        atual.setEmail((String) actualTable.getValue(2, "email"));
+        atual.setEndereco((String) actualTable.getValue(2, "endereco"));
+        atual.setNome((String) actualTable.getValue(2, "nome"));
+        atual.setTelefone((String) actualTable.getValue(2, "telefone"));
+        atual.setId((int) actualTable.getValue(2, "id"));
+
+        Cliente esperado = new Cliente("Jonas", "jonas@mail.com", "3109231321", "asdasdas", "123.123.423-93");
+        esperado.setId(10);
+
+        Assert.assertEquals(esperado.getCpf(), atual.getCpf());
+        Assert.assertEquals(esperado.getNome(), atual.getNome());
+        Assert.assertEquals(esperado.getEndereco(), atual.getEndereco());
+        Assert.assertEquals(esperado.getEmail(), atual.getEmail());
+        Assert.assertEquals(esperado.getTelefone(), atual.getTelefone());
+        Assert.assertEquals(esperado, atual);
+
     }
 
     @Test
