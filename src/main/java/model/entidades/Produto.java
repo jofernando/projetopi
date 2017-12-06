acad4ec2c61d0acc8a261a7310fbaf3342bc7923
José Fernando Mendes <fmdinho3@gmail.com>
2017-12-06 08:48:25 -0300

    Formatando

--- src/main/java/model/entidades/Produto.java
+++ src/main/java/model/entidades/Produto.java
@@ -18,10 +18,10 @@ import javax.persistence.Id;
 public class Produto implements Serializable {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@Id
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
     @GeneratedValue
     private int id;
     private String codigo;
@@ -29,70 +29,70 @@ public class Produto implements Serializable {
     private double preco;
 
     public Produto(String codigo, String nome, double preco) {
-	this.codigo = codigo;
-	this.nome = nome;
-	this.preco = preco;
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
     }
 
     public Produto() {
     }
 
     public int getId() {
-	return id;
+        return id;
     }
 
     public String getCodigo() {
-	return codigo;
+        return codigo;
     }
 
     public void setCodigo(String codigo) {
-	this.codigo = codigo;
+        this.codigo = codigo;
     }
 
     public String getNome() {
-	return nome;
+        return nome;
     }
 
     public void setNome(String nome) {
-	this.nome = nome;
+        this.nome = nome;
     }
 
     public double getPreco() {
-	return preco;
+        return preco;
     }
 
     public void setPreco(double preco) {
-	this.preco = preco;
+        this.preco = preco;
     }
 
     @Override
     public int hashCode() {
-	int hash = 7;
-	hash = 83 * hash + this.id;
-	return hash;
+        int hash = 7;
+        hash = 83 * hash + this.id;
+        return hash;
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (this == obj) {
-	    return true;
-	}
-	if (obj == null) {
-	    return false;
-	}
-	if (getClass() != obj.getClass()) {
-	    return false;
-	}
-	final Produto other = (Produto) obj;
-	if (this.id != other.id) {
-	    return false;
-	}
-	return true;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
-	return "Produto{" + "id=" + id + ", codigo=" + codigo + ", nome=" + nome + ", preco=" + preco + '}';
+        return "Produto{" + "id=" + id + ", codigo=" + codigo + ", nome=" + nome + ", preco=" + preco + '}';
     }
 
 }
