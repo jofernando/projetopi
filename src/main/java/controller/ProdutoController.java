5d67b232a1aa9f5925e14eeaddfc36179f27d14e
José Fernando Mendes <fmdinho3@gmail.com>
2017-12-06 08:47:09 -0300

    Adicionando variaveis para melhor gerenciamento da view

--- src/main/java/controller/ProdutoController.java
+++ src/main/java/controller/ProdutoController.java
@@ -23,90 +23,115 @@ public class ProdutoController {
 
     private ProdutoModel model = null;
     private Produto produto;
-    private boolean estadoBusca;
+    private boolean busca;
+    private boolean cadastro;
+    private boolean alteracao;
 
     public ProdutoController() {
-	model = new ProdutoModel(ProdutoModel.BANCODADOS);
-	produto = new Produto();
-	estadoBusca = true;
+        model = new ProdutoModel(ProdutoModel.BANCODADOS);
+        produto = new Produto();
+        busca = true;
     }
 
     public void inserirAction() {
-	if (model.inserir(produto)) {
-	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto cadastrado", null));
-	}
+        if (model.inserir(produto)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto cadastrado", null));
+        }
     }
 
     public void alterarAction() {
-	if (model.alterar(produto)) {
-	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto alterado", null));
-	}
+        if (model.alterar(produto)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto alterado", null));
+        }
     }
 
     public void deletarAction() {
-	if (model.deletar(produto)) {
-	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto deletado", null));
-	}
+        if (model.deletar(produto)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto deletado", null));
+        }
     }
 
     public void deletarAction(Produto produto) {
-	if (model.deletar(produto)) {
-	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto deletado", null));
-	}
+        if (model.deletar(produto)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Produto deletado", null));
+        }
     }
 
     public Produto buscarAction(int id) {
-	return model.buscar(id);
+        return model.buscar(id);
     }
 
     public List<Produto> buscarTodosAction() {
-	return model.buscarTodos();
+        return model.buscarTodos();
     }
 
     public Produto buscarPorCpfAction(String codigo) {
-	return model.buscarPorCodigo(codigo);
+        return model.buscarPorCodigo(codigo);
     }
 
     public Produto getProduto() {
-	return produto;
+        return produto;
     }
 
     public void setProduto(Produto produto) {
-	this.produto = produto;
+        this.produto = produto;
     }
 
     public void limpar() {
-	produto = new Produto();
-    }
-
-    public boolean isEstadoBusca() {
-	return estadoBusca;
-    }
-
-    public void setEstadoBusca(boolean estadoBusca) {
-	this.estadoBusca = estadoBusca;
+        produto = null;
+        produto = new Produto();
     }
 
     public void preparaBuscar() {
-	limpar();
-	estadoBusca = true;
+        limpar();
+        busca = true;
+        cadastro = false;
+        alteracao = false;
     }
 
     public void preparaCadastrar() {
-	limpar();
-	estadoBusca = false;
+        limpar();
+        cadastro = true;
+        busca = false;
+        alteracao = false;
     }
 
     public void preparaAlterar(Produto produto) {
-	this.produto = produto;
-	estadoBusca = false;
+        this.produto = null;
+        this.produto = produto;
+        alteracao = true;
+        busca = false;
+        cadastro = false;
     }
 
-    public void preparaSalvar() {
-	if (produto.getId() == 0) {
-	    inserirAction();
-	} else {
-	    alterarAction();
-	}
+//    public void preparaSalvar() {
+//        if (produto.getId() == 0) {
+//            inserirAction();
+//        } else {
+//            alterarAction();
+//        }
+//    }
+    public boolean isCadastro() {
+        return cadastro;
+    }
+
+    public void setCadastro(boolean cadastro) {
+        this.cadastro = cadastro;
+    }
+
+    public boolean isAlteracao() {
+        return alteracao;
+    }
+
+    public void setAlteracao(boolean alteracao) {
+        this.alteracao = alteracao;
+    }
+
+    public boolean isBusca() {
+        return busca;
+    }
+
+    public void setBusca(boolean busca) {
+        this.busca = busca;
     }
 }
